c5cdf09cf0cfb5b577671bb584b4c2ee017d2005
Bertrand Chardon <bertrand.chardon@gmail.com>
2013-01-15 19:47:50 -0800

    Added basic grep on directory
    Basically spawns a goroutine for each file in the argument directory.
    A channel of strings is created at the beginning of the function so that
    each goroutine can report the matches to the caller.
    
    When all the files have been grep'd, return to the main().

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
   "fmt"
   "net/http"
+  "strings"
   "log"
-  "io"
+  "io/ioutil"
+  "path/filepath"
   "os"
-  "bufio"
 )
 
 // Handles the initial GET on /
@@ -69,38 +70,123 @@ func searchPostHandler(w http.ResponseWriter, r *http.Request) {
   }
 }
 
-// Signature copied from fmt.Printf
-// See http://golang.org/pkg/fmt/#Printf
-func PrintFile(fileName string) (n int, err error) {
-  file, err := os.Open("/Users/bertrand/diff_branches.txt")
-  if err != nil {
-    fmt.Printf("Error while opening a file: ", err)
-  }
-  defer file.Close()
-  fileReader := bufio.NewReader(file)
-  for {
-    line, err := fileReader.ReadString('\n')
-    if err != nil {
-      if err != io.EOF {
-        fmt.Println(err)
-      }
-      break
+func CountFiles(fi []os.FileInfo) (count int) {
+  count = 0
+  for _, elem := range fi {
+    if !elem.IsDir() {
+      count++
     }
-    fmt.Print(line)
   }
-  // Faking it for now, I still don't know how to "save" 
-  // that piece of code in a function
-  return 100, nil
+  return
 }
 
-func main() {
+// Greps all the file in a given directory and returns the results 
+// as an array of strings
+func DirGrep(directoryName string, searchTerm string) (matches []string) {
 
-  http.HandleFunc("/", searchGetHandler)
-  // Not clear exactly what should be used to discriminate between GETs and POSTs
-  // Maybe http.Request.Method??
-  http.HandleFunc("/search", searchPostHandler)
-  err := http.ListenAndServe(":8080", nil)
+  // Acquire a file handle
+  dir, err := os.Open(directoryName)
+  defer dir.Close()
   if err != nil {
-    log.Fatal("ListenAndServe: ", err)
+    fmt.Println(err)
+    os.Exit(1)
   }
+  // Gets the contents of the directory (files and subdirectories)
+  contents, err := dir.Readdir(0)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+  leftToProcess := CountFiles(contents)
+  // Create a channel so that we can grep all the files concurrently and synchronize
+  // (Whatever that means)
+  ch := make(chan string)
+
+  for _, f := range contents {
+    // If the entry is a file, start a new goroutine
+    // and write on the channel when a match is found
+    // If it's a directory we want to 
+    // recursively call the DirGrep function on it
+    if f.IsDir() {
+      fmt.Printf("\n\nDIRECTORY: %s\n\n", filepath.Join("/Users", "bertrand", "Programming", "wendigo", "test", f.Name()))
+      continue
+    } else {
+      fmt.Printf("\n\nFILE: %s\n\n", filepath.Join("/Users", "bertrand", "Programming", "wendigo", "test", f.Name()))
+      go func(fileName string) {
+        stringContents, err := ioutil.ReadFile(filepath.Join("/Users", "bertrand", "Programming", "wendigo", "test", fileName))
+        if err != nil {
+          fmt.Println(err)
+          os.Exit(1)
+        }
+        lines := strings.Split(string(stringContents), "\n")
+        for _, line := range lines {
+          if strings.Contains(line, searchTerm) {
+            ch <- fmt.Sprintf("Found the search term in file @@ %s @@\n\n", fileName)
+          }
+        }
+        leftToProcess--
+      }(f.Name())
+    }
+  }
+  for {
+    select {
+      case m, ok := <-ch:
+        if !ok {
+          fmt.Println("Channel is closed")
+        } else {
+          matches = append(matches, m)
+          if leftToProcess == 0 {
+            goto END
+          }
+        }
+    }
+  }
+END:
+  return matches
+}
+
+// TODO: Pass a string channel as a parameter and stuff 
+// the results in there for the caller to consume
+// Signature copied from fmt.Printf
+// See http://golang.org/pkg/fmt/#Printf
+/* func PrintFile(fileName string) (n int, err error) { */
+/*   file, err := os.Open(fileName) */
+/*   if err != nil { */
+/*     fmt.Printf("Error while opening a file: ", err) */
+/*   } */
+/*   defer file.Close() */
+/*   fileReader := bufio.NewReader(file) */
+/*   lineNumber := 1 */
+/*   for { */
+/*     line, err := fileReader.ReadString('\n') */
+/*     if err != nil { */
+/*       if err != io.EOF { */
+/*         fmt.Println(err) */
+/*       } */
+/*       break */
+/*     } */
+/*     if strings.Contains(line, "work_week") { */
+/*       fmt.Printf("Term \"%s\" found at line %d\n", "work_week", lineNumber) */
+/*     } */
+/*     lineNumber++ */
+/*   } */
+/*   // Faking it for now, I still don't know how to "save" */
+/*   // that piece of code in a function */
+/*   return 100, nil */
+/* } */
+
+func main() {
+  found := DirGrep("/Users/bertrand/Programming/wendigo/test", "FOOBAR")
+
+  for _, match := range found {
+    fmt.Printf("\n\nMATCH: %s\n\n", match)
+  }
+  /* http.HandleFunc("/", searchGetHandler) */
+  /* // Not clear exactly what should be used to discriminate between GETs and POSTs */
+  /* // Maybe http.Request.Method?? */
+  /* http.HandleFunc("/search", searchPostHandler) */
+  /* err := http.ListenAndServe(":8080", nil) */
+  /* if err != nil { */
+  /*   log.Fatal("ListenAndServe: ", err) */
+  /* } */
 }
