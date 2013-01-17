06d041363c7188a1c34bb6b178569ca0c7a30d21
Bertrand Chardon <bertrand.chardon@gmail.com>
2013-01-17 00:56:03 -0800

    #go-nuts curated version of the crawler

--- main.go
+++ main.go
@@ -1,226 +1,45 @@
 package main
+
 import (
-  "fmt"
-  "net/http"
-  "strings"
-  "log"
-  "io/ioutil"
-  "path/filepath"
-  "os"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 )
 
-// Handles the initial GET on /
-func searchGetHandler(w http.ResponseWriter, r *http.Request) {
-  // FIXME: Use templates for all this
-  searchForm := `
-    <html>
-      <head>
-      </head>
-      <body>
-        <form action="/search" method="POST">
-          <div>
-            <input name="term" size="60" />
-          </div>
-          <div>
-            <input name="directory" size="60" />
-          </div>
-          <div>
-            <input type="submit" value="Search">
-          </div>
-        </form>
-      </body>
-    </html>
-  `
-  fmt.Fprintf(w, searchForm)
+func walk(path string, filepaths chan<- string) {
+	dir, err := os.Open(path)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer dir.Close()
+	contents, err := dir.Readdir(0)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	for _, f := range contents {
+		path := filepath.Join(path, f.Name())
+		if f.IsDir() {
+			WalkPathRecursively(path, filepaths)
+		} else {
+			filepaths <- path
+		}
+	}
 }
 
-// TODO: This should be hooked to the main chunk of the (i.e. once the directory
-// and the search term have been obtained, we should launch the crawling)
-
-// Handles POST requests to /search
-func searchPostHandler(w http.ResponseWriter, r *http.Request) {
-  directoryName, searchTerm := r.FormValue("directory"), r.FormValue("term")
-
-  fmt.Println(directoryName)
-  fmt.Println(searchTerm)
-
-  dirHandle, err := os.Open(directoryName)
-
-  // Ensure that the file descriptor gets properly closed
-  defer dirHandle.Close()
-
-  if err != nil {
-    log.Fatal("Error while trying to open the directory: ", err)
-    os.Exit(1)
-  }
-  // Read all the contents of the directory at once 
-  // (See http://golang.org/pkg/os/#File.Readdir)
-  files, err := dirHandle.Readdir(0)
-  if err != nil {
-    log.Fatal("Error while reading the contents of the directory: ", err)
-  }
-  for _, f := range files {
-    // Sorry son, you got a bad case of ugly
-    if f.IsDir() {
-      fmt.Fprintf(w, "%s is a directory\n", f.Name())
-    } else {
-      fmt.Fprintf(w, "%s is a file\n", f.Name())
-    }
-  }
+func recursiveWalk(path string, filepaths chan<- string) {
+	walk(path, filepaths)
+	close(filepaths)
 }
-
-func CountFiles(directoryName string) (count int) {
-  count = 0
-  dir, err := os.Open(directoryName)
-  defer dir.Close()
-  if err != nil {
-    fmt.Printf("\nError in CountFiles while trying to open directory %s\n", directoryName)
-    os.Exit(1)
-  }
-  contents, err := dir.Readdir(0)
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
-  for _, elem := range contents {
-    if elem.IsDir() {
-      count += CountFiles(filepath.Join(directoryName, elem.Name()))
-    } else {
-      // We migth want to handle all the possible cases here see http://golang.org/pkg/os/#FileMode
-      count++
-    }
-  }
-  return
-}
-
-// Greps all the file in a given directory and returns the results 
-// as an array of strings
-func DirGrep(directoryName string, searchTerm string, leftToProcess int) (matches []string) {
-  // Acquire a file handle
-  fmt.Printf("\n    %d files left to process\n", leftToProcess)
-  dir, err := os.Open(directoryName)
-  defer dir.Close()
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
-  // Gets the contents of the directory (files and subdirectories)
-  contents, err := dir.Readdir(0)
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
-  // Create a channel so that we can grep all the files concurrently and synchronize
-  // (Whatever that means)
-  ch := make(chan string)
-
-  for _, f := range contents {
-    // If the entry is a file, start a new goroutine
-    // and write on the channel when a match is found
-    // If it's a directory we want to 
-    // recursively call the DirGrep function on it
-    if f.IsDir() {
-      // Gets the contents of the directory (files and subdirectories)
-      fmt.Printf("\nEntering directory %s\n", filepath.Join(directoryName, f.Name()))
-      DirGrep(filepath.Join(directoryName, f.Name()), searchTerm, leftToProcess)
-    } else {
-      fmt.Printf("\n* Processing file %s\n", filepath.Join(directoryName, f.Name()))
-      go func(fileName string) {
-        stringContents, err := ioutil.ReadFile(filepath.Join(directoryName, fileName))
-        if err != nil {
-          fmt.Println(err)
-          os.Exit(1)
-        }
-        lines := strings.Split(string(stringContents), "\n")
-        for _, line := range lines {
-          if strings.Contains(line, searchTerm) {
-            ch <- fmt.Sprintf("\n!!! Found the search term in file %s\n", fileName)
-          }
-        }
-        leftToProcess--
-        fmt.Printf("\n    Finished processing %s, %d files left to process", fileName, leftToProcess)
-      }(f.Name())
-    }
-  }
-  for {
-    select {
-      case m, ok := <-ch:
-        if !ok {
-          fmt.Println("Channel is closed")
-        } else {
-          matches = append(matches, m)
-          if leftToProcess == 0 {
-            goto END
-          }
-        }
-    }
-  }
-END:
-  return matches
-}
-
-// TODO: Pass a string channel as a parameter and stuff 
-// the results in there for the caller to consume
-// Signature copied from fmt.Printf
-// See http://golang.org/pkg/fmt/#Printf
-/* func PrintFile(fileName string) (n int, err error) { */
-/*   file, err := os.Open(fileName) */
-/*   if err != nil { */
-/*     fmt.Printf("Error while opening a file: ", err) */
-/*   } */
-/*   defer file.Close() */
-/*   fileReader := bufio.NewReader(file) */
-/*   lineNumber := 1 */
-/*   for { */
-/*     line, err := fileReader.ReadString('\n') */
-/*     if err != nil { */
-/*       if err != io.EOF { */
-/*         fmt.Println(err) */
-/*       } */
-/*       break */
-/*     } */
-/*     if strings.Contains(line, "work_week") { */
-/*       fmt.Printf("Term \"%s\" found at line %d\n", "work_week", lineNumber) */
-/*     } */
-/*     lineNumber++ */
-/*   } */
-/*   // Faking it for now, I still don't know how to "save" */
-/*   // that piece of code in a function */
-/*   return 100, nil */
-/* } */
 
 func main() {
-  /* dir, err := os.Open("/Users/bertrand/Programming/wendigo/test") */
-  /* defer dir.Close() */
-  /* if err != nil { */
-  /*   fmt.Println(err) */
-  /*   os.Exit(1) */
-  /* } */
-  /* // Gets the contents of the directory (files and subdirectories) */
-  /* contents, err := dir.Readdir(0) */
-  /* if err != nil { */
-  /*   fmt.Println(err) */
-  /*   os.Exit(1) */
-  /* } */
+	dirName := "/Users/bertrand/Programming/wendigo/test"
+	filepaths := make(chan string)
+	go recursiveWalk(dirName, filepaths)
 
-  /* fmt.Println(CountFiles(contents)) */
-  dirName := "/Users/bertrand/Programming/wendigo/test"
-  searchTerm := "FOOBAR"
-
-  found := DirGrep(dirName, searchTerm, CountFiles(dirName))
-
-  for _, match := range found {
-    fmt.Printf("\n\nMATCH: %s\n\n", match)
-  }
-
-
-
-  /* http.HandleFunc("/", searchGetHandler) */
-  /* // Not clear exactly what should be used to discriminate between GETs and POSTs */
-  /* // Maybe http.Request.Method?? */
-  /* http.HandleFunc("/search", searchPostHandler) */
-  /* err := http.ListenAndServe(":8080", nil) */
-  /* if err != nil { */
-  /*   log.Fatal("ListenAndServe: ", err) */
-  /* } */
+	for path := range filepaths {
+		fmt.Println(path)
+	}
 }
