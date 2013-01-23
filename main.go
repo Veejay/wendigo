c11c61b7520ba2b8c3b9c284ebc1f0cdfb38526e
Bertrand Chardon <bertrand.chardon@gmail.com>
2013-01-22 20:49:18 -0800

    New revamped version of the grep part
    Need to piece this all back together (re-integrate the server part and
    the pygmentize part as well).
    
    It'd be nice to be able to pass a directory to the grep as well.
    And add error handling.

--- main.go
+++ main.go
@@ -1,97 +1,87 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
-	"sync"
+  "fmt"
+  "strings"
+  "os"
+  "io/ioutil"
+  "runtime"
 )
 
+type Search struct {
+  filename string
+  matches chan<-FileMatch
+}
+
 type FileMatch struct {
-	fileName    string
-	lineNumber  int
-	matchRegion []string
+  filename string
+  linenumber int
+  match string
 }
 
-func (match FileMatch) String() string {
-	return strings.Join(match.matchRegion, "\n")
-}
-
-func searchTerm(filepath string, term string) (matches []FileMatch) {
-	contents, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	lines := strings.Split(string(contents), "\n")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	for lineNumber, line := range lines {
-		if strings.Contains(line, term) {
-			matches = append(matches, FileMatch{fileName: filepath, lineNumber: lineNumber, matchRegion: lines[lineNumber-2 : lineNumber+2]})
-		}
-	}
-	return matches
-}
-
-func Pygmentize(snippet string) (pygmentedSnippet string) {
-
-	var out bytes.Buffer
-	pygmentize := exec.Command("pygmentize", "-f", "html", "-l", "ruby")
-
-	pygmentize.Stdin = strings.NewReader(snippet)
-	pygmentize.Stdout = &out
-
-	pygmentize.Start()
-
-	pygmentize.Wait()
-
-	return string(out.Bytes())
-}
+// Number of workers we'll be spawning
+var workers = runtime.NumCPU()
 
 func main() {
-	directoryPath := "/Users/bertrand/Programming/wendigo/test"
+  term := os.Args[1]
+  findMatches(term, os.Args[2:])
+}
 
-	// Empty slice of strings, will eventually contain the paths to the files
-	// gathered by the recursive walk of the directory
-	paths := []string{}
-	var wg sync.WaitGroup
 
-	filepath.Walk(directoryPath, func(path string, fi os.FileInfo, err error) error {
-		if err != nil {
-			panic(err)
-		}
-		// TODO: Take care of the symbolic links as well 
-		// See os.ModeSymlink
-		if !fi.IsDir() {
-			paths = append(paths, path)
-		}
-		return nil
-	})
+func findMatches(term string, filenames []string) {
+  searches := make(chan Search, workers)
+  done := make(chan bool, workers)
+  matches := make(chan FileMatch, len(filenames))
 
-	for _, path := range paths {
-		wg.Add(1)
-		// Calls Done() on the waiting to indicate that the task has been completed
-		// but since the work is not being done sequentially, the goroutine itself needs to send that 
-		// signal
-		go func(path string) {
-			matches := searchTerm(path, "assignments")
-			for _, match := range matches {
-				fmt.Println(Pygmentize(match.String()))
-			}
-			wg.Done()
-		}(path)
-	}
-	// Since all the jobs are launched in goroutines, nothing will block
-	// needs to wait for everything to finish before exiting the program 
-	wg.Wait()
-	fmt.Println("Done processing all the files")
+  go queueSearches(searches, matches, filenames) // Send all the searches on the searches channel 
+
+  for i := 0; i < workers; i++ {
+    go executeSearches(term, done, searches) // Get the searches from the channel and execute them
+  }
+  // Wait for all the workers to be totally done (i.e. they have consumed everything on the searches channel)
+  go tallyCompleted(matches, done)
+
+  // Will try to read from the matches channel until it's closed, which
+  // will happen when each single worker is done with its workload
+  for match := range matches {
+    // TODO: Add a formatter 
+    fmt.Printf("Result:\t%#v\n", match)
+  }
+  fmt.Println("DONE")
+}
+
+func queueSearches(searches chan<-Search, matches chan<-FileMatch, filenames []string) {
+  for _, filename := range filenames {
+    searches<-Search{filename: filename, matches: matches}
+  }
+  close(searches)
+}
+
+func executeSearches(term string, done chan<-bool, searches <-chan Search) {
+  // Will keep on reading on the channel until a value arrives
+  for search := range searches {
+	  contents, err := ioutil.ReadFile(search.filename)
+	  if err != nil {
+      panic(err)
+	  }
+	  lines := strings.Split(string(contents), "\n")
+	  if err != nil {
+      panic(err)
+	  }
+	  for n, line := range lines {
+		  if strings.Contains(line, term) {
+			  search.matches <- FileMatch{filename: search.filename, linenumber: n, match: line}
+		  }
+	  }
+  }
+  // The channel is dry to the bone, let's bounce
+  done <- true
+}
+
+func tallyCompleted(matches chan FileMatch, done <-chan bool) {
+  // Once all the workers have sent their "done" signal, we can close the results channel and exit the program
+  for w := 0; w < workers; w++ {
+    <-done
+  }
+  close(matches)
 }
